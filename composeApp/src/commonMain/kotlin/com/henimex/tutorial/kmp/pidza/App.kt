412a4f5f0773a989fec970cc8759998d3d502881
henimex-ISM <henimex@gmail.com>
2025-07-30 16:06:20 +0300

    Pizza Order Screen

--- composeApp/src/commonMain/kotlin/com/henimex/tutorial/kmp/pidza/App.kt
+++ composeApp/src/commonMain/kotlin/com/henimex/tutorial/kmp/pidza/App.kt
@@ -1,21 +1,43 @@
 package com.henimex.tutorial.kmp.pidza
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.henimex.tutorial.kmp.pidza.designFiles.MainColor
+import com.henimex.tutorial.kmp.pidza.designFiles.TypeColor1
+import com.henimex.tutorial.kmp.pidza.designFiles.TypeColor2
+import com.henimex.tutorial.kmp.pidza.designFiles.getItalicFontFamily
 import com.henimex.tutorial.kmp.pidza.designFiles.getMainFontFamily
+import kmppidza.composeapp.generated.resources.Res
+import kmppidza.composeapp.generated.resources.pizza_third
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
-@Preview
+@Preview()
 fun App() {
     MaterialTheme {
         Column(
@@ -23,7 +45,7 @@ fun App() {
                 .safeContentPadding()
                 .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+            verticalArrangement = Arrangement.SpaceEvenly
         ) {
             Text(
                 text = "Pizza",
@@ -31,6 +53,105 @@ fun App() {
                 fontSize = 46.sp,
                 fontFamily = getMainFontFamily()
             )
+
+            Image(painter = painterResource(Res.drawable.pizza_third), "")
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Button(
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = MainColor,
+                        contentColor = TypeColor1
+                    )
+                ) {
+                    Text(text = "Cheese")
+                }
+
+                Button(
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = MainColor,
+                        contentColor = TypeColor1
+                    )
+                ) {
+                    Text(text = "Sausage")
+                }
+
+                Button(
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = MainColor,
+                        contentColor = TypeColor1
+                    )
+                ) {
+                    Text(text = "Olive")
+                }
+            }
+
+            Text(
+                text = "20 min",
+                color = TypeColor2,
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = "Delivery",
+                color = MainColor,
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = "Meat lover, get ready to meet your pizza!",
+                color = TypeColor2,
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold,
+                fontFamily = getItalicFontFamily(),
+                textAlign = TextAlign.Center
+            )
+
+            HorizontalDivider(thickness = 1.dp, color = Color.Black)
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Row(
+                modifier = Modifier
+                    .padding(5.dp)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+
+            ) {
+                //Text(text = "$ 5.75", fontSize = 24.sp, fontWeight = FontWeight.Bold, color = MainColor)
+
+                Button(
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = TypeColor1,
+                        contentColor = MainColor
+                    ),
+                    modifier = Modifier.border(
+                        width = 1.dp,
+                        color = MainColor,
+                        shape = RoundedCornerShape(24.dp)
+                    )
+                ) {
+                    Text(text = "$ 5.75", fontSize = 24.sp, fontWeight = FontWeight.Bold)
+                }
+
+                Button(
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = MainColor,
+                        contentColor = TypeColor1
+                    )
+                ) {
+                    Text(text = "Add To Cart", fontSize = 24.sp, fontWeight = FontWeight.Bold)
+                }
+            }
+
         }
     }
 }
